920896f9f047d6b91ee79ce7e84853f9b36905ba
lijingqi <411803345@qq.com>
2018-08-15 09:12:38 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        项目开始
+      </div>
+    );
+  }
+}
+
+export default App;
